028e9221cf62fcd80e627603b2929ca6d6b86d02
Michaela <michis.j@hotmail.com>
2022-02-04 13:25:49 +0100

    Hasse was here

---
+++ Inlämningsuppgift2Webbsäkerhet/Models/ChatMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Inlämningsuppgift2Webbsäkerhet.Models
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Message { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
